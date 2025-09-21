48f3e78e463ba062d8ff9b6d08b744efa8808787
utku-13 <ozerutku13@gmail.com>
2025-09-21 21:04:42 +0300

    for long results from API we made serialization async too better practice ofc

--- TurkishTechCase/ViewModel/ListViewViewModel.swift
+++ TurkishTechCase/ViewModel/ListViewViewModel.swift
@@ -12,19 +12,24 @@ class ListViewViewModel: ObservableObject {
     @Published var items: [ListItem] = []
 
     func fetchItems() async {
-        do {
-            let data = try await AF.request("https://fakestoreapi.com/products")
-                .validate()
-                .serializingData()
-                .value
-            
-            if let products = [ListItem].fromJSON(data) { // decode içine yazdığımız extensionu kullandık burada.
-                await MainActor.run {
-                    self.items = products
-                }
+    do {
+        let data = try await AF.request("https://fakestoreapi.com/products")
+            .validate()
+            .serializingData()
+            .value
+        
+        // Move JSON decoding to background
+        let products = await Task.detached {
+            return [ListItem].fromJSON(data)
+        }.value
+        
+        if let products = products {
+            await MainActor.run {
+                self.items = products
             }
-        } catch {
-            print("API Error:", error)
         }
+    } catch {
+        print("API Error:", error)
     }
+}
 }
